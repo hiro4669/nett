4136475ebf48d7aa143c6656ea1116deea208d46
Hiroaki Fukuda <hiroaki@java2.jp>
2018-12-24 20:57:41 +0900

    add analyze arp

--- pcap/analyze.c
+++ pcap/analyze.c
@@ -3,7 +3,20 @@
 #include <arpa/inet.h>
 #include <net/ethernet.h> // struct ether_header
 
+
 #include "pcap.h"
+
+int analyze_arp(struct ether_arp *eth_arp, int size) {
+
+    if (size < sizeof(struct ether_arp)) {
+        fprintf(stderr, "size(%d) is less than arp header(%d)\n", size, (int)sizeof(struct ether_arp));
+        return -1;
+    }
+    print_arp(eth_arp, stdout);
+
+    return 0;
+}
+
 
 int analyze_packet(uint8_t* data, int size) {
 
@@ -15,11 +28,14 @@ int analyze_packet(uint8_t* data, int size) {
     lest = size;
     e_head = (struct ether_header*)ptr;
 
-//    printf("%04x\n", ntohs(e_head->ether_type));
+    ptr += sizeof(struct ether_header);
+    lest -= sizeof(struct ether_header);
 
+    fprintf(stderr, "\nPacket[%dbytes]\n", size);
     switch(ntohs(e_head->ether_type)) {
         case ETHERTYPE_ARP: {
             print_ether_header(e_head, stdout);
+            analyze_arp((struct ether_arp*)ptr, lest);
             break;
         }
         case ETHERTYPE_IP: {
--- pcap/print.c
+++ pcap/print.c
@@ -5,6 +5,58 @@
 
 #include "pcap.h"
 
+/* hardware type from /usr/include/net/if_arp.h */
+static char *hrd[] = {
+    "From KA9Q: NET/ROM pseudo",
+    "Ethernet 10/100Mbps.",
+    "Experimental Ethernet",
+    "AX.25 Level 2",
+    "PROnet token ring",
+    "Chaosnet.",
+    "IEEE 802.2 Ethernet/TR/TB."
+    "ARCnet",
+    "APPLEtalk"
+    "undefined",
+    "undefined",
+    "undefined",
+    "undefined",
+    "undefined",
+    "undefined"
+    "Frame Relay DLCI"
+    "undefined",
+    "undefined",
+    "undefined",
+    "ATM",
+    "undefined",
+    "undefined",
+    "undefined",
+    "Metricom STRIP (new IANA id)"
+    "IEEE 1394 IPv4 - RFC 2734"
+    "undefined",
+    "undefined",
+    "EUI-64",
+    "undefined",
+    "undefined",
+    "undefined",
+    "undefined",
+    "InfiniBand"
+};
+
+static char *opcode[] = {
+    "undefined",
+    "ARP Request",
+    "ARP Reply",
+    "RARP Request",
+    "RARP Reply",
+    "undefined",
+    "undefined",
+    "undefined",
+    "InARP Request",
+    "InARP Reply",
+    "(ATM)ARP NAK"
+};
+
+/* show mac address */
 static char* ether_ntoa_r(uint8_t* hwaddr, char* buf, socklen_t size) {
     snprintf(buf, size, "%02x:%02x:%02x:%02x:%02x:%02x",
         hwaddr[0],
@@ -16,6 +68,64 @@ static char* ether_ntoa_r(uint8_t* hwaddr, char* buf, socklen_t size) {
     );
     return buf;
 }
+
+/* show ip address */
+static char* arp_ip2str(uint8_t* ip, char *buf, socklen_t size) {
+    snprintf(buf,size,"%u.%u.%u.%u",ip[0],ip[1],ip[2],ip[3]);
+    return(buf);
+}
+
+int print_arp(struct ether_arp* eth_arp, FILE *fp) {
+    char buf[80];
+
+    fprintf(fp, "arp-----------------------------------\n");
+    fprintf(fp, "arp_hrd=%u", ntohs(eth_arp->arp_hrd));
+    if (ntohs(eth_arp->arp_hrd) <= 32) {
+        fprintf(fp, "(%s),", hrd[ntohs(eth_arp->arp_hrd)]);
+    } else {
+        fprintf(fp, "(unknown/undefined),");
+    }
+    fprintf(fp, "ar_pro = %u", ntohs(eth_arp->arp_pro));
+    switch(ntohs(eth_arp->arp_pro)) {
+        case ETHERTYPE_IP: {
+            fprintf(fp, "(IP)\n");
+            break;
+        }
+        case ETHERTYPE_ARP: {
+            fprintf(fp, "(ARP)\n");
+            break;
+        }
+        case ETHERTYPE_REVARP: {
+            fprintf(fp, "(Reverse ARP)\n");
+            break;
+        }
+        case ETHERTYPE_IPV6: {
+            fprintf(fp, "(IPV6)\n");
+            break;
+        }
+        default: {
+            fprintf(fp, "(unknown)\n");
+            break;
+        }
+    }
+    fprintf(fp, "arp_hwd_len = %u,", eth_arp->arp_hln);
+    fprintf(fp, "arp_pro_len = %u\n", eth_arp->arp_pln);
+    fprintf(fp, "arp_op      = %u,", ntohs(eth_arp->arp_op));
+    if (ntohs(eth_arp->arp_op) <= 10) {
+        fprintf(fp, "(%s)\n", opcode[ntohs(eth_arp->arp_op)]);
+    } else {
+        fprintf(fp, "(undefined)\n");
+    }
+
+    fprintf(fp, "arp_sha = %s\n", ether_ntoa_r(eth_arp->arp_sha, buf, sizeof(buf)));
+    fprintf(fp, "arp_spa = %s\n", arp_ip2str(eth_arp->arp_spa, buf, sizeof(buf)));
+    fprintf(fp, "arp_tha = %s\n", ether_ntoa_r(eth_arp->arp_tha, buf, sizeof(buf)));
+    fprintf(fp, "arp_tpa = %s\n", arp_ip2str(eth_arp->arp_tpa, buf, sizeof(buf)));
+
+    return 0;
+}
+
+
 
 int print_ether_header(struct ether_header *eh, FILE *fp) {
     char buf[80];
@@ -42,7 +152,5 @@ int print_ether_header(struct ether_header *eh, FILE *fp) {
             break;
         }
     }
-
-
     return 0;
 }
